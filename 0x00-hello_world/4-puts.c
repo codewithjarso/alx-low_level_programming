7632a884551145c0149ca1c0f66fc926779ffc73
codewithjarso <stephenjarso6@gmail.com>
2023-06-23 02:43:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 /**
- * program that prints using put instead of print f
+ *main- program that prints using put instead of print f
  *return:0 if success
  */
 int main(void)
-/*
- * print output to screen
- */{
-	 /*
-	  * print to screen
-	  */
+
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
