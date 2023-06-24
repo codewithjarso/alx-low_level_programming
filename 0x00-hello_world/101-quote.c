1a1838a5ba3c7e56e3a81ed2a6caf07e7c5fadd1
codewithjarso <stephenjarso6@gmail.com>
2023-06-24 09:44:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - c program tha prints a line to the standard unix
+ * main - c program that prints a line to the standard unix
  * Return: 1 always (Success)
  */
 int main(void)
 {
-	fputf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
