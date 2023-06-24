e74018dd14ae0454212984ae737ed6c8d0f31121
codewithjarso <stephenjarso6@gmail.com>
2023-06-24 00:27:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints a statement to the screen using printf
+ * return (0) - if it is a sucess
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
