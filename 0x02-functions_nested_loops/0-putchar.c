229d4d984078a2109707deed6af3cfca4d20a72a
codewithjarso <stephenjarso6@gmail.com>
2023-06-26 09:43:25 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 	#include <unistd.h>
 	/**
@@ -10,4 +11,45 @@
 	int _putchar(char c)
 	{
 		return (write(1, &c, 1));
+	}
+#ifndef main_h
+	#define main_h
+
+
+	int _putchar(char);
+	void print_alphabet(void);
+	void print_alphabet_x10(void);
+	int _islower(int c);
+	int _isalpha(int c);
+	int print_sign(int n);
+	int _abs(int);
+	int print_last_digit(int);
+	void jack_bauer(void);
+	void times_table(void);
+	int add(int, int);
+	void print_to_98(int n);
+	void print_times_table(int n);
+
+
+	#endif
+#include "main.h"
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
+
+
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 	}
