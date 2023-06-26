e801f6bd1e5e8a29ce9d7a153478377091a9c210
codewithjarso <stephenjarso6@gmail.com>
2023-06-26 08:32:04 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in lowercase begining from z
+ * Return: 0 if it is a success
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch <= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n);
+	return(0);
+}
