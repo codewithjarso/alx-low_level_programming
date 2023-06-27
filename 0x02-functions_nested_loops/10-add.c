b8134a129fed3990993facdbd884268248f24630
codewithjarso <stephenjarso6@gmail.com>
2023-06-26 23:27:33 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 
-	/**
-	 * add: - function that adds the two integers
-	 * @a: int to be added to b
-	 * @b: int to be added to a
-	 * Return: sum of a and b
-	 */
+/**
+ * add - function that adds the two integers
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
 	int add(int a, int b)
 	{
 		int c;
