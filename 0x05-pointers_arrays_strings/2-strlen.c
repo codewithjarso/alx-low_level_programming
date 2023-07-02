2b5bd1b695714de7fa572618598d993cbc2536b8
codewithjarso <stephenjarso6@gmail.com>
2023-07-02 01:55:10 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,16 @@
 /**
  * _strlen - get the length of a string
  * @s:input of the string
+ * Return: length
  */
 int _strlen(char *s)
 {
-	strlen(s);
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
