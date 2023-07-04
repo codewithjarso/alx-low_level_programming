2eea1a7ba218384d43325c54e1d9186cb307a98e
codewithjarso <stephenjarso6@gmail.com>
2023-07-04 02:53:21 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,12 @@ void more_numbers(void)
 
 	for (i = 0; i <= 10;; i++)
 	{
-		for (c = '0'; c <= '14'; c++)
+		for (c = '0'; c <= '14';)
 		{
-			_putchar(c);
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
