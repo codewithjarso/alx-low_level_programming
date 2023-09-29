0ae50f18c2c13dfdde828aecf1e72fae0ace9804
codewithjarso <stephenjarso6@gmail.com>
2023-09-29 06:31:44 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc:number of arguments
+ * @argv:array of arguments
+ * Return:0 success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
