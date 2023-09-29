5d8078c8fff50e21c493c72d7887500bb8f9eff8
codewithjarso <stephenjarso6@gmail.com>
2023-09-29 07:12:30 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * @argc: argument counter
+ * @argv: array of arguments
+ * Return: o on success 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+
+	return (0);
+}
