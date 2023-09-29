ba553fd26db9755fdefb244a08db1d4fdd59a9a1
codewithjarso <stephenjarso6@gmail.com>
2023-09-29 06:09:55 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -  entry point
+ * @argc:argument counter
+ * @argv:argument vector
+ * Return:0 if success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
