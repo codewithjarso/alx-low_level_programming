eaf43a88cbbe81f6ad6e9c29b181e4649433cf04
codewithjarso <stephenjarso6@gmail.com>
2023-11-04 02:42:05 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ * _strdup - duplicate to new memory location
+ * @str:initial value
+ * ptr:pointer to duplicate str
+ * Return: 0 on success
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int r = 0;
+	char *ptr = (char *)malloc(i * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	if (ptr == NULL)
+		return (NULL);
+	for (r = 0; str[r]; r++)
+		ptr[r] = str[r];
+	return (ptr);
+}
