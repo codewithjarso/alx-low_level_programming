5654fd383f393c3ffdc1e2b4fbdaaba1d34742a8
codewithjarso <stephenjarso6@gmail.com>
2023-11-07 14:17:39 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,9 +1,9 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * free_dog - frees dog
- * @d:pointer to dog
- */
+  * free_dog - frees dog
+  * @d:pointer to dog
+  */
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
