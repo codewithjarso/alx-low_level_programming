55608a30f873b423eb836ccab98745a49032a7a1
codewithjarso <stephenjarso6@gmail.com>
2023-11-07 14:26:21 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,23 +13,23 @@ int _strlen(char *s)
 	i = 0;
 	while (s[i] != '\0')
 	{
-			i++;
-		}
+		i++;
+	}
 
 
 		return (i);
 	}
 
 
-	/**
-	 * _strcpy - copies the string pointed to by src
-	 * including the terminating null byte (\0)
-	 * to the buffer pointed to by dest
-	 * @dest: pointer to the buffer in which we copy the string
-	 * @src: string to be copied
-	 *
-	 * Return: the pointer to dest
-	 */
+/**
+ * _strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
 	char *_strcpy(char *dest, char *src)
 	{
 		int len, i;
@@ -55,14 +55,14 @@ int _strlen(char *s)
 	}
 
 
-	/**
-	 * new_dog - creates a new dog
-	 * @name: name of the dog
-	 * @age: age of the dog
-	 * @owner: owner of the dog
-	 *
-	 * Return: pointer to the new dog (Success), NULL otherwise
-	 */
+/**
+ * *new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
 	dog_t *new_dog(char *name, float age, char *owner)
 	{
 		dog_t *dog;
