5e735bf6ba8e74c17c36b6673bf92950879871da
codewithjarso <stephenjarso6@gmail.com>
2023-11-07 06:29:40 -0800

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog -function
+ * @d: dog pointer
+ * @name: name of dog
+ * @age:age of dog
+ * @owner: owner name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
