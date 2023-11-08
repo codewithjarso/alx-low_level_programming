6892d450116d8373d4480d3c2fb17971268dd759
codewithjarso <stephenjarso6@gmail.com>
2023-11-08 05:30:28 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - function to print name on the screen
+ * @name:name to be printed
+ * @f:pointer to a function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
