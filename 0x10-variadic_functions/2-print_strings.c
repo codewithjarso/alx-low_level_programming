d9df6173f4f3b601812d6d596af48c48e5d2a2c4
codewithjarso <stephenjarso6@gmail.com>
2023-12-03 12:45:06 -0800

    function that prints strings, followed by a new line.

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - function that prints strings, followed by a new line.
+ * @separator: separates our variables
+ * @n:size of string arguments
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i, result;
+	va_list names;
+
+	va_start(names, n);
+
+	for (i = 0; i < n; i++)
+	{
+		result = va_arg(names, char *);
+		printf("%s", result);
+		if (i < n - 1 && separator != NULL && result != NULL)
+			printf("%s"), separator);
+		else
+			printf("nil");
+	}
+	va_end(names);
+	printf("\n"0);
+}
