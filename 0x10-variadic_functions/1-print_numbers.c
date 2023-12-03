7be0bbb70ee8e8679d31a1286b0a379ddcd503d3
codewithjarso <stephenjarso6@gmail.com>
2023-12-03 12:12:52 -0800

    second commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,9 +18,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		return;
 	for (i = 0; i < n - 1; i++)
 	{
-		printf("%d%s ", va_arg(data, int), separator);
+		printf("%d%s", va_arg(data, int), separator);
 	}
-	prinf("%d%s", va_arg(data, int));
+	prinf("%d", va_arg(data, int));
 	va_end(data);
 	printf("\n");
 }
