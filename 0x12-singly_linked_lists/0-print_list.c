45ef5ac763ca8c0fc0239cb751417fb255183f0b
codewithjarso <stephenjarso6@gmail.com>
2023-12-07 11:19:58 -0800

    second commit fixing betty issues

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
 		h = h->next;
 		count++;
 	}
