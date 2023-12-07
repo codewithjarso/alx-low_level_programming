a918ae8d11769ad32aac5251666ba26f831ad211
codewithjarso <stephenjarso6@gmail.com>
2023-12-07 11:02:53 -0800

    second commit fixing betty issues

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,9 +10,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t list_len(const list_t *h);
 int _putchar(char c);
