e14ec2d0b354a821ab5b4a2bf9a91ea4dfc2888b
codewithjarso <stephenjarso6@gmail.com>
2023-12-08 02:00:42 -0800

    minor fixes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,7 +17,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (!new)
 		return (NULL);
-	new->str = str;
+	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
 	(*head) = new;
