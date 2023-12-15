a7772bd5e8ec17024d8887626460b388dd7755b0
codewithjarso <stephenjarso6@gmail.com>
2023-12-15 07:01:09 -0800

    function to return binary from a decimal number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_binary - function to convert decimal into binary
+ * @n: decimal to be converted
+ * Return: binary number
+ */
+void print_binary(unsigned long int n)
+{
+   int i = 1;
+
+   while (n != NULL)
+   {
+      result = n & i;
+
+      if (n & 1)
+      {
+         _putchar('1');
+      }
+      else
+      {
+         _putchar('0');
+      }
+      n =  n >> 1;
+   }
+}
