b58ee4a0e4de93f1c2211f29e9948dd8f73fd3b7
codewithjarso <stephenjarso6@gmail.com>
2023-12-15 04:12:13 -0800

    bit manipulatin

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to _int - function to coonvert binary to decimal
+ * @b:pointer to the binary number
+ * Return: 0 if null and the decimal number if not
+ */
+unsigned int binary_to_uint(const char *b)
+{
+   int result = 0;
+   int len = strlen(b);
+
+   if (b == NULL)
+      return (0);
+
+   for (int i = 0;i<len; i++)
+   {
+      if (b[i] == '1')
+         result = result * 2 + 1;
+      else if (b[i] == '0')
+         result = result * 2;
+      else
+         return (0);
+   }
+   return (result)
+}
